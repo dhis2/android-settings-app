64f38b26b0c669ca4aa052045c0547e715fd213e
Milagros Rodriguez <milagros@krowdy.co>
2019-06-19 15:03:10 -0500

    fix: removing JSONS adding constants folder

---
+++ src/constants/android-settings.js
@@ -0,0 +1,41 @@
+export const metadataOptions = [
+    {
+        value: '1h',
+        label: '1h',
+    },
+    {
+        value: '12h',
+        label: '12h',
+    },
+    {
+        value: '1d',
+        label: '1d',
+    },
+    {
+        value: '7d',
+        label: '7d',
+    },
+]
+
+export const dataOptions = [
+    {
+        value: '30m',
+        label: '30m',
+    },
+    {
+        value: '1h',
+        label: '1h',
+    },
+    {
+        value: '6h',
+        label: '6h',
+    },
+    {
+        value: '12h',
+        label: '12h',
+    },
+    {
+        value: '24h',
+        label: '24h',
+    },
+]
---
+++ src/constants/program-settings.js
@@ -0,0 +1,381 @@
+export const Program = [
+    {
+        keyDownload: 'settingDownload',
+        keyDBTrimming: 'settingDBTrimming',
+        option: 'Setting for',
+        download: [
+            {
+                label: 'Global',
+                value: 'global',
+            },
+            {
+                label: 'Per Org Unit',
+                value: 'orgUnit',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Global',
+                value: 'global',
+            },
+            {
+                label: 'Per Org Unit',
+                value: 'orgUnit',
+            },
+        ],
+    },
+    {
+        option: 'TEI per program',
+        download: '500',
+        DBTrimming: '500',
+    },
+    {
+        keyDownload: 'enrollmentDownload',
+        keyDBTrimming: 'enrollmentDBTrimming',
+        option: 'TEI Enrollments',
+        download: [
+            {
+                label: 'All',
+                value: 'all',
+            },
+            {
+                label: 'Only Active',
+                value: 'onlyActive',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'All',
+                value: 'all',
+            },
+            {
+                label: 'Only Active',
+                value: 'onlyActive',
+            },
+        ],
+    },
+    {
+        keyDownload: 'enrollmentDateDownload',
+        keyDBTrimming: 'enrollmentDateDBTrimming',
+        option: 'TEI Enrollment date',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+    {
+        keyDownload: 'updateDownload',
+        keyDBTrimming: 'updateDBTrimming',
+        option: 'TEI last update',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+    {
+        option: 'TE reserved values',
+        download: '100',
+        DBTrimming: '500',
+    },
+    {
+        option: 'Events',
+        download: '1000',
+        DBTrimming: '1000',
+    },
+    {
+        keyDownload: 'eventDownload',
+        keyDBTrimming: 'eventDBTrimming',
+        option: 'Event period',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+]
+
+export const SpecificProgram = [
+    {
+        keyDownload: 'specificSettingDownload',
+        keyDBTrimming: 'specificSettingDBTrimming',
+        option: 'Setting for',
+        download: [
+            {
+                label: 'Global',
+                value: 'global',
+            },
+            {
+                label: 'Per Org Unit',
+                value: 'orgUnit',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Global',
+                value: 'global',
+            },
+            {
+                label: 'Per Org Unit',
+                value: 'orgUnit',
+            },
+        ],
+    },
+    {
+        option: 'TEI per program',
+        download: '500',
+        DBTrimming: '500',
+    },
+    {
+        keyDownload: 'specificEnrollmentDownload',
+        keyDBTrimming: 'specificEnrollmentDBTrimming',
+        option: 'TEI Enrollments',
+        download: [
+            {
+                label: 'All',
+                value: 'all',
+            },
+            {
+                label: 'Only Active',
+                value: 'onlyActive',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'All',
+                value: 'all',
+            },
+            {
+                label: 'Only Active',
+                value: 'onlyActive',
+            },
+        ],
+    },
+    {
+        keyDownload: 'specificEnrollmentDateDownload',
+        keyDBTrimming: 'specificEnrollmentDateDBTrimming',
+        option: 'TEI Enrollment date',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+    {
+        keyDownload: 'specificUpdateDownload',
+        keyDBTrimming: 'specificUpdateDBTrimming',
+        option: 'TEI last update',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+    {
+        option: 'TE reserved values',
+        download: '100',
+        DBTrimming: '500',
+    },
+    {
+        option: 'Events',
+        download: '1000',
+        DBTrimming: '1000',
+    },
+    {
+        keyDownload: 'specificEventDownload',
+        keyDBTrimming: 'specificEventDBTrimming',
+        option: 'Event period',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+]
