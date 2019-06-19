f24bb035367003cb19a46d92afd7dfe91f529e56
Milagros Rodriguez <milagros@krowdy.co>
2019-06-19 05:14:03 -0500

    fix: adding jsons for table options

---
+++ src/JSONS/data-set-settings.js
@@ -0,0 +1,87 @@
+export const DataSetting = [
+    {
+        keyDownload: 'periodDSDownload',
+        keyDBTrimming: 'periodDSDBTrimming',
+        option: 'Periods',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+]
+
+export const DataSpecificSetting = [
+    {
+        keyDownload: 'specificPeriodDSDownload',
+        keyDBTrimming: 'specificPeriodDSDBTrimming',
+        option: 'Periods',
+        download: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+        DBTrimming: [
+            {
+                label: 'Any',
+                value: 'any',
+            },
+            {
+                label: 'Last month',
+                value: 'lastMonth',
+            },
+            {
+                label: 'Last 3 months',
+                value: 'last3Month',
+            },
+            {
+                label: 'Last 12 months',
+                value: 'last12Month',
+            },
+        ],
+    },
+]
