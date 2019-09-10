2912da2cd565603dc75ca55b7b9fff7f4e7beb66
Milagros Rodriguez <milagros@krowdy.co>
2019-09-09 21:47:57 -0500

    fix: change value constants for formatByte size

--- src/utils/memory-size.js
+++ src/utils/memory-size.js
@@ -37,7 +37,8 @@ export function memorySizeOf(obj) {
 export function formatByteSize(bytes) {
     if (bytes < 1024) return bytes
     // + " bytes";
-    else if (bytes < 1048576) return Math.round(bytes / 1024) //.toFixed(3) //+ " KB";
+    else return Math.round(bytes / 1024)
+    // else if (bytes < 1048576) return Math.round(bytes / 1024) //.toFixed(3) //+ " KB";
     /* else if (bytes < 1073741824) return (bytes / 1048576).toFixed(3) + " MB";
     else return (bytes / 1073741824).toFixed(3) + " GB"; */
 }
