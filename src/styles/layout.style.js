dfbffcc754d68b0c12ea6d2c9d8d29c5fc87c5bd
Milagros Rodriguez <milagros@krowdy.co>
2019-12-04 12:23:19 -0500

    feat: create css in js

---
+++ src/styles/layout.style.js
@@ -0,0 +1,27 @@
+const styles = {
+    paper__layout: {
+        padding: '2rem 2.5rem',
+        minHeight: '300px',
+        marginTop: '40px',
+        maxWidth: '1200px',
+    },
+    paper__twoPanel__main: {
+        marginTop: '3rem',
+    },
+    paper__twoPanel__mainTitle: {
+        fontWeight: 300,
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: '24px',
+        color: 'rgba(0, 0, 0, 0.87)',
+        padding: '16px 0px 5px',
+    },
+    paper__twoPanel__sideBar: {
+        position: 'absolute',
+    },
+    button__rightCorner: {
+        margin: '40px 0px 10px 0px',
+        float: 'right',
+    },
+}
+
+export default styles
---
+++ src/styles/settings.style.js
@@ -0,0 +1,104 @@
+const style = {
+    dataTable: {
+        marginBottom: '2rem',
+        fontFamily: 'Roboto, sans-serif',
+    },
+    dataTable__headers__header: {
+        fontFamily: 'Roboto, sans-serif',
+        verticalAlign: 'middle',
+        borderBottom: '2px solid #E0E0E0', //!important
+        color: '#757575', //!important
+        display: 'table-cell',
+        fontWeight: '400',
+        textAlign: 'left',
+        transition: 'all .3s ease',
+    },
+    dataTable__rows__row__column: {
+        fontFamily: 'Roboto, sans-serif',
+        verticalAlign: 'middle',
+        borderBottom: '1px solid #E0E0E0', //!important
+        color: '#212121',
+        borderTop: 0,
+        display: 'table-cell',
+        textAlign: 'left',
+        transition: 'all .3s ease',
+        position: 'relative',
+    },
+    mainContent__title: {
+        fontFamily: 'Roboto, sans-serif',
+        fontStyle: 'normal',
+        fontWeight: '600',
+        fontSize: '15px',
+        lineHeight: '19px',
+        color: 'rgba(51, 51, 51, 0.87)',
+        marginBottom: 0,
+    },
+    mainContent__title__main: {
+        fontFamily: 'Roboto, sans-serif',
+        fontSize: '16px',
+        marginBottom: 'initial',
+        fontStyle: 'normal',
+        fontWeight: '600',
+        lineHeight: '19px',
+        color: 'rgba(51, 51, 51, 0.87)',
+    },
+    mainContent__subtitle: {
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: 'normal',
+        fontSize: '16px',
+        lineHeight: '24px',
+        fontStyle: 'normal',
+        color: 'rgba(51, 51, 51, 0.87)',
+        marginBottom: 0,
+    },
+    mainContent__title__dialog: {
+        fontStyle: 'italic',
+        fontSize: '16px',
+        fontWeight: '400',
+        fontFamily: 'Roboto, sans-serif',
+        lineHeight: '19px',
+        color: 'rgba(51, 51, 51, 0.87)',
+        marginBottom: 0,
+    },
+    mainContent__button__container: {
+        fontFamily: 'Roboto, sans-serif',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        padding: '4rem 1rem 1rem',
+    },
+    mainContent__dialog__button: {
+        fontFamily: 'Roboto, sans - serif',
+        marginRight: '10px',
+    },
+    data__topMargin: {
+        marginTop: '20px',
+    },
+    subitemTitle: {
+        color: '#494949',
+        padding: 0,
+        fontSize: '0.8rem',
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: 'bold',
+    },
+    subitemItem: {
+        color: '#494949',
+        padding: 0,
+        fontSize: '0.8rem',
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: '500',
+    },
+    subitemBigitem: {
+        color: '#494949',
+        padding: 0,
+        fontFamily: 'Roboto, sans-serif',
+        fontWeight: '500',
+        fontSize: '1.1rem',
+        textAlign: 'center',
+    },
+    maxValue: {
+        color: 'red',
+    },
+}
+
+export default style
