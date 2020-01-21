626a1d68cedef424b6af9033f297d4e862c3ded8
Milagros Rodriguez <milagros@krowdy.co>
2020-01-21 16:41:27 -0500

    fix: adding styles to table

--- src/components/table-actions.js
+++ src/components/table-actions.js
@@ -11,6 +11,8 @@ import {
 } from '@dhis2/ui-core'
 import IconButton from '@material-ui/core/IconButton'
 import { DeleteOutlined, CreateOutlined } from '@material-ui/icons'
+import i18n from '@dhis2/d2-i18n'
+import dataTableStyles from '../styles/DataTable.module.css'
 
 const TableActions = ({ columns, rows, menuActions }) => {
     return (
@@ -26,22 +28,27 @@ const TableActions = ({ columns, rows, menuActions }) => {
                         </TableCellHead>
                     ))}
                     <TableCellHead dataTest="dhis2-uicore-tablecellhead">
-                        Actions
+                        {i18n.t('Actions')}
                     </TableCellHead>
                 </TableRowHead>
             </TableHead>
             <TableBody dataTest="dhis2-uicore-tablebody">
                 {rows.map(row => (
                     <TableRow dataTest="dhis2-uicore-tablerow" key={row.id}>
-                        <TableCell dataTest="dhis2-uicore-tablecell">
+                        <TableCell
+                            dataTest="dhis2-uicore-tablecell"
+                            className={dataTableStyles.dataTable_row_title}
+                        >
                             {row.name}
                         </TableCell>
-                        <TableCell dataTest="dhis2-uicore-tablecell">
+                        <TableCell
+                            dataTest="dhis2-uicore-tablecell"
+                            className={dataTableStyles.dataTable_row_title}
+                        >
                             {row.sumarySettings}
                         </TableCell>
 
                         <TableCell dataTest="dhis2-uicore-tablecell" dense>
-                            {/* <TableButton /> */}
                             <IconButton
                                 onClick={() => {
                                     menuActions.edit(row)
