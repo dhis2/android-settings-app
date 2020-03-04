44199b7eab5776c829da9d5eea9c7868335db002
Milagros Rodriguez <milagros@krowdy.co>
2020-03-04 17:41:33 -0500

    fix: keynames from strings to constants general setting

--- src/components/android-settings-container.js
+++ src/components/android-settings-container.js
@@ -10,6 +10,7 @@ import {
     maxValues,
 } from '../constants/android-settings'
 import AndroidSettings from './android-settings'
+import { NAMESPACE, GENERAL_SETTINGS } from '../constants/data-store'
 
 const { metadataSync, dataSync, encryptDB } = androidSettingsDefault
 
@@ -88,19 +89,15 @@ class AndroidSettingsContainer extends React.Component {
     }
 
     saveDataApi = data => {
-        this.keyName === 'android_settings'
+        this.keyName === GENERAL_SETTINGS
             ? api
-                  .updateValue('ANDROID_SETTING_APP', 'android_settings', data)
+                  .updateValue(NAMESPACE, GENERAL_SETTINGS, data)
                   .then(res => res)
             : api
-                  .getKeys('ANDROID_SETTING_APP')
+                  .getKeys(NAMESPACE)
                   .then(
                       api
-                          .createValue(
-                              'ANDROID_SETTING_APP',
-                              'android_settings',
-                              data
-                          )
+                          .createValue(NAMESPACE, GENERAL_SETTINGS, data)
                           .then(res => res)
                   )
     }
@@ -125,19 +122,17 @@ class AndroidSettingsContainer extends React.Component {
         await api
             .getNamespaces()
             .then(res => {
-                const nameSpace = res.filter(
-                    name => name === 'ANDROID_SETTING_APP'
-                )
+                const nameSpace = res.filter(name => name === NAMESPACE)
                 nameSpace.length === 0
                     ? (this.nameSpace = undefined)
                     : (this.nameSpace = nameSpace[0])
                 this.nameSpace !== undefined
                     ? this.setState({ isUpdated: true })
                     : this.setState({ isUpdated: false })
-                if (this.nameSpace === 'ANDROID_SETTING_APP') {
+                if (this.nameSpace === NAMESPACE) {
                     api.getKeys(this.nameSpace).then(res => {
                         const keyName = res.filter(
-                            name => name === 'android_settings'
+                            name => name === GENERAL_SETTINGS
                         )
                         keyName.length === 0
                             ? (this.keyName = undefined)
@@ -154,18 +149,14 @@ class AndroidSettingsContainer extends React.Component {
                                       })
                                   })
                             : api
-                                  .updateValue(
-                                      'ANDROID_SETTING_APP',
-                                      'android_settings',
-                                      {
-                                          metadataSync: metadataSync,
-                                          dataSync: dataSync,
-                                          numberSmsToSend: '',
-                                          numberSmsConfirmation: '',
-                                          valuesTEI: '',
-                                          encryptDB: encryptDB,
-                                      }
-                                  )
+                                  .createValue(NAMESPACE, GENERAL_SETTINGS, {
+                                      metadataSync: metadataSync,
+                                      dataSync: dataSync,
+                                      numberSmsToSend: '',
+                                      numberSmsConfirmation: '',
+                                      valuesTEI: '',
+                                      encryptDB: encryptDB,
+                                  })
                                   .then(res => {
                                       this.setState({
                                           metadataSync: metadataSync,
@@ -174,16 +165,15 @@ class AndroidSettingsContainer extends React.Component {
                                           numberSmsConfirmation: '',
                                           valuesTEI: '',
                                           encryptDB: encryptDB,
+                                          isUpdated: true,
+                                          loading: false,
                                       })
                                   })
                     })
                 } else if (this.nameSpace === undefined) {
-                    api.createNamespace(
-                        'ANDROID_SETTING_APP',
-                        'android_settings'
-                    )
+                    api.createNamespace(NAMESPACE, GENERAL_SETTINGS)
                         .then(res => {
-                            this.keyName = 'android_settings'
+                            this.keyName = GENERAL_SETTINGS
                             this.setState({
                                 isUpdated: true,
                                 loading: false,
---
+++ src/constants/data-store.js
@@ -0,0 +1,4 @@
+export const NAMESPACE = 'ANDROID_SETTING_APP'
+export const GENERAL_SETTINGS = 'general_settings'
+export const PROGRAM_SETTINGS = 'program_settings'
+export const DATASET_SETTINGS = 'dataSet_settings'
