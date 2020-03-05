5dd1f71edbdefe25ecd90a5bf37eb1aca7364954
Milagros Rodriguez <milagros@krowdy.co>
2020-03-05 12:11:47 -0500

    fix: change value type from select options to input number

--- src/constants/data-set-settings.js
+++ src/constants/data-set-settings.js
@@ -3,42 +3,6 @@ export const DataSetting = [
         keyDownload: 'periodDSDownload',
         keyDBTrimming: 'periodDSDBTrimming',
         option: 'Periods',
-        download: [
-            {
-                label: 'Any',
-                value: 'ANY',
-            },
-            {
-                label: 'Last month',
-                value: 'LAST_MONTH',
-            },
-            {
-                label: 'Last 3 months',
-                value: 'LAST_3_MONTHS',
-            },
-            {
-                label: 'Last 12 months',
-                value: 'LAST_12_MONTHS',
-            },
-        ],
-        DBTrimming: [
-            {
-                label: 'Any',
-                value: 'ANY',
-            },
-            {
-                label: 'Last month',
-                value: 'LAST_MONTH',
-            },
-            {
-                label: 'Last 3 months',
-                value: 'LAST_3_MONTHS',
-            },
-            {
-                label: 'Last 12 months',
-                value: 'LAST_12_MONTHS',
-            },
-        ],
     },
 ]
 
@@ -47,46 +11,10 @@ export const DataSpecificSetting = [
         keyDownload: 'periodDSDownload',
         keyDBTrimming: 'periodDSDBTrimming',
         option: 'Periods',
-        download: [
-            {
-                label: 'Any',
-                value: 'ANY',
-            },
-            {
-                label: 'Last month',
-                value: 'LAST_MONTH',
-            },
-            {
-                label: 'Last 3 months',
-                value: 'LAST_3_MONTHS',
-            },
-            {
-                label: 'Last 12 months',
-                value: 'LAST_12_MONTHS',
-            },
-        ],
-        DBTrimming: [
-            {
-                label: 'Any',
-                value: 'ANY',
-            },
-            {
-                label: 'Last month',
-                value: 'LAST_MONTH',
-            },
-            {
-                label: 'Last 3 months',
-                value: 'LAST_3_MONTHS',
-            },
-            {
-                label: 'Last 12 months',
-                value: 'LAST_12_MONTHS',
-            },
-        ],
     },
 ]
 
 export const DataSetSettingsDefault = {
-    periodDSDownload: 'LAST_12_MONTHS',
-    periodDSDBTrimming: 'LAST_12_MONTHS',
+    periodDSDownload: 12,
+    periodDSDBTrimming: 12,
 }
