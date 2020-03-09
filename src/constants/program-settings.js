275b2930dda20c3174544c4155d62763cb8cb02a
Milagros Rodriguez <milagros@krowdy.co>
2020-03-09 15:18:06 -0500

    fix: specific settings programs default values

--- src/constants/program-settings.js
+++ src/constants/program-settings.js
@@ -415,3 +415,20 @@ export const ProgramSettingsDefault = {
     eventDateDownload: 'ANY',
     eventDateDBTrimming: 'ANY',
 }
+
+export const SpecificSettingsDefault = {
+    settingDownload: 'ALL_ORG_UNITS',
+    settingDBTrimming: 'ALL_ORG_UNITS',
+    teiDownload: 500,
+    teiDBTrimming: 500,
+    enrollmentDownload: 'ALL',
+    enrollmentDBTrimming: 'ALL',
+    enrollmentDateDownload: 'ANY',
+    enrollmentDateDBTrimming: 'ANY',
+    updateDownload: 'ANY',
+    updateDBTrimming: 'ANY',
+    eventsDownload: 1000,
+    eventsDBTrimming: 1000,
+    eventDateDownload: 'ANY',
+    eventDateDBTrimming: 'ANY',
+}
