0e5845e665f46fa656ac40328176a094e5698479
Milagros Rodriguez <milagros@krowdy.co>
2020-03-12 23:18:58 -0500

    feat: hide DBTrimming options dataset

--- src/components/dataSet-settings.js
+++ src/components/dataSet-settings.js
@@ -36,8 +36,8 @@ class DataSetSettings extends React.Component {
     }
 
     state = {
-        periodDSDownload: periodDSDownload,
-        periodDSDBTrimming: periodDSDBTrimming,
+        periodDSDownload,
+        periodDSDBTrimming,
         specificSetting: {
             openDialog: false,
             periodDSDBTrimming: '',
@@ -59,11 +59,11 @@ class DataSetSettings extends React.Component {
         edit: (...args) => {
             this.dataSetToChange = args[0].name
             const argsData = args[0]
+            const settings = this.populateObject('SPECIFIC', argsData)
             this.setState({
                 specificSetting: {
+                    ...settings,
                     name: argsData.id,
-                    periodDSDownload: argsData.periodDSDownload,
-                    periodDSDBTrimming: argsData.periodDSDBTrimming,
                     openDialog: true,
                 },
             })
@@ -142,6 +142,35 @@ class DataSetSettings extends React.Component {
         }
     }
 
+    populateObject = (type, settingsList) => {
+        let object
+        switch (type) {
+            case 'GLOBAL':
+                object = {
+                    periodDSDownload: settingsList.periodDSDownload,
+                }
+                break
+            case 'SPECIFIC':
+                object = {
+                    periodDSDownload: settingsList.periodDSDownload,
+                }
+                break
+            case 'DEFAULT':
+                object = {
+                    periodDSDownload,
+                }
+                break
+            case 'CLEAN':
+                object = {
+                    periodDSDownload: '',
+                }
+                break
+            default:
+                break
+        }
+        return object
+    }
+
     /**
      * Updates global settings on Fly
      */
@@ -150,10 +179,10 @@ class DataSetSettings extends React.Component {
             return true
         }
 
+        const settings = this.populateObject('GLOBAL', this.state)
         const globalSettings = {
+            ...settings,
             lastUpdated: new Date().toJSON(),
-            periodDSDownload: this.state.periodDSDownload,
-            periodDSDBTrimming: this.state.periodDSDBTrimming,
         }
 
         this.globalSettings = globalSettings
@@ -203,12 +232,11 @@ class DataSetSettings extends React.Component {
 
     handleClose = () => {
         this.dataSetToChange = undefined
-
+        const settings = this.populateObject('CLEAN')
         this.setState({
             specificSetting: {
+                ...settings,
                 openDialog: false,
-                periodDSDownload: '',
-                periodDSDBTrimming: '',
                 name: '',
             },
         })
@@ -228,13 +256,15 @@ class DataSetSettings extends React.Component {
         )
 
         if (dataSetNameFilter.length > 0) {
+            const settings = this.populateObject(
+                'SPECIFIC',
+                this.state.specificSetting
+            )
             objData[specificDataSetNameKey] = {
+                ...settings,
                 id: specificDataSetNameKey,
                 lastUpdated: new Date().toJSON(),
                 name: dataSetNameFilter[0].name,
-                periodDSDownload: this.state.specificSetting.periodDSDownload,
-                periodDSDBTrimming: this.state.specificSetting
-                    .periodDSDBTrimming,
             }
 
             const sumarySettings =
@@ -242,12 +272,8 @@ class DataSetSettings extends React.Component {
                     ? undefined
                     : this.state.specificSetting.periodDSDownload
             const newDataSetRow = {
-                name: dataSetNameFilter[0].name,
-                sumarySettings: sumarySettings,
-                periodDSDownload: this.state.specificSetting.periodDSDownload,
-                periodDSDBTrimming: this.state.specificSetting
-                    .periodDSDBTrimming,
-                id: specificDataSetNameKey,
+                ...objData[specificDataSetNameKey],
+                sumarySettings,
             }
 
             this.specificSettings = objData
@@ -281,9 +307,9 @@ class DataSetSettings extends React.Component {
     }
 
     handleReset = () => {
+        const settings = this.populateObject('DEFAULT')
         this.setState({
-            periodDSDownload: periodDSDownload,
-            periodDSDBTrimming: periodDSDBTrimming,
+            ...settings,
         })
         this.updateGlobal = true
     }
@@ -377,13 +403,8 @@ class DataSetSettings extends React.Component {
                                                         : dataSet.periodDSDownload
 
                                                 const newDataSetRow = {
-                                                    name: dataSet.name,
-                                                    sumarySettings: sumarySettings,
-                                                    periodDSDownload:
-                                                        dataSet.periodDSDownload,
-                                                    periodDSDBTrimming:
-                                                        dataSet.periodDSDBTrimming,
-                                                    id: key,
+                                                    ...dataSet,
+                                                    sumarySettings,
                                                 }
 
                                                 this.specificSettingsRows.push(
@@ -408,9 +429,9 @@ class DataSetSettings extends React.Component {
                                 }
                             )
                         } else {
+                            const settings = this.populateObject('DEFAULT')
                             this.globalSettings = {
-                                periodDSDownload: periodDSDownload,
-                                periodDSDBTrimming: periodDSDBTrimming,
+                                ...settings,
                             }
 
                             const data = {
