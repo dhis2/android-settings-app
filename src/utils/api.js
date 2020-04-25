907c5f8075aaed0a84445675af6762a6c4e2a188
Milagros Rodriguez <milagros@krowdy.co>
2020-04-24 23:32:48 -0500

    fix: adding return to api createNamespace function

--- src/utils/api.js
+++ src/utils/api.js
@@ -24,17 +24,13 @@ class Api {
         )
     }
 
-    createNamespace(namespace, key, value) {
+    createNamespace(namespace, key, value = {}) {
         return getInstance()
             .then(d2 => {
-                console.log(d2.dataStore)
                 d2.dataStore.create(namespace).then(namespace => {
-                    value == undefined
-                        ? namespace.set(key, {})
-                        : namespace.set(key, value)
+                    return namespace.set(key, value)
                 })
             })
-            .then(name => console.log(name))
             .catch(error => Promise.reject(error))
     }
 
