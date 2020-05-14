79c6f2378e822b2d7872ca92c90fc281a66aec22
Milagros Rodriguez <milagros@krowdy.co>
2020-05-13 23:48:43 -0500

    fix: clean row component

--- src/components/settings-table/program-table-row.js
+++ src/components/settings-table/program-table-row.js
@@ -4,11 +4,9 @@ import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
-import { RadioGroup, Radio } from '@dhis2/ui-core'
 import InputNumber from '../input-number'
 import i18n from '@dhis2/d2-i18n'
 import dataTableStyles from '../../styles/DataTable.module.css'
-import radioStyles from '../../styles/Input.module.css'
 import tableTitleStyles from '../../styles/TableTitle.module.css'
 import {
     specificSettingsDefault,
@@ -40,42 +38,23 @@ const ProgramTableRow = ({ dataRow, states, onChange }) => {
                 align="right"
             >
                 {Array.isArray(dataRow.download) === true ? (
-                    dataRow.radioButton === true ? (
-                        <RadioGroup
-                            dense
-                            name={dataRow.keyDownload}
-                            id={dataRow.keyDownload}
-                            onChange={onChange}
-                            value={states[dataRow.keyDownload]}
-                            className={radioStyles.container_content_inline}
-                        >
-                            {dataRow.download.map(option => (
-                                <Radio
-                                    key={option.value}
-                                    label={option.label}
-                                    value={option.value}
-                                />
-                            ))}
-                        </RadioGroup>
-                    ) : (
-                        <Select
-                            key={dataRow.keyDownload}
-                            value={states[dataRow.keyDownload]}
-                            onChange={onChange}
-                            id={dataRow.keyDownload}
-                            name={dataRow.keyDownload}
-                        >
-                            {dataRow.download.map(option => (
-                                <MenuItem
-                                    value={option.value}
-                                    key={option.value}
-                                    name={option.value}
-                                >
-                                    <em> {option.label} </em>
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    )
+                    <Select
+                        key={dataRow.keyDownload}
+                        value={states[dataRow.keyDownload]}
+                        onChange={onChange}
+                        id={dataRow.keyDownload}
+                        name={dataRow.keyDownload}
+                    >
+                        {dataRow.download.map(option => (
+                            <MenuItem
+                                value={option.value}
+                                key={option.value}
+                                name={option.value}
+                            >
+                                <em> {option.label} </em>
+                            </MenuItem>
+                        ))}
+                    </Select>
                 ) : (
                     <InputNumber
                         name={dataRow.keyDownload}
