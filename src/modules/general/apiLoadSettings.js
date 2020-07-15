a2e6aa9fea5bb1afc0e2c8f3adf14301810e9b9a
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2020-07-14 22:11:24 -0500

    fix: cleaning callbacks general settings

--- src/modules/general/apiLoadSettings.js
+++ src/modules/general/apiLoadSettings.js
@@ -3,23 +3,39 @@ import { GENERAL_SETTINGS, NAMESPACE } from '../../constants/data-store'
 import { getAuthority } from '../getAuthority'
 
 /**
- * When general settings component mount, get namespace and keys from dataStore and return value
+ * Check if dataStore has ANDROID SETTINGS namespace and GENERAL_SETTINGS key, get general settings from dataStore
+ * */
+const getGeneralSettings = async authority => {
+    const hasAuthority = authority
+
+    return api
+        .getNamespaces()
+        .then(
+            namespaces =>
+                namespaces.includes(NAMESPACE) && api.getKeys(NAMESPACE)
+        )
+        .then(
+            keys =>
+                keys.includes(GENERAL_SETTINGS) &&
+                getSettingsFromDataStore(hasAuthority)
+        )
+        .then(settings => settings)
+}
+
+/**
+ * Get general settings from dataStore and add disable flag in case user has authority
+ * */
+const getSettingsFromDataStore = hasAuthority => {
+    return api.getValue(NAMESPACE, GENERAL_SETTINGS).then(settings => {
+        const generalSettings = settings.value
+        generalSettings.disableAll = !hasAuthority
+        return generalSettings
+    })
+}
+
+/**
+ * When general settings component mount, check if user has ALL authority, get namespace and keys from dataStore and return value
  */
 export const apiLoadGeneralSettings = async () => {
-    return getAuthority().then(hasAuthority => {
-        return api.getNamespaces().then(res => {
-            if (res.includes(NAMESPACE)) {
-                return api.getKeys(NAMESPACE).then(res => {
-                    if (res.includes(GENERAL_SETTINGS)) {
-                        return api
-                            .getValue(NAMESPACE, GENERAL_SETTINGS)
-                            .then(res => {
-                                res.value.disableAll = !hasAuthority
-                                return res.value
-                            })
-                    }
-                })
-            }
-        })
-    })
+    return getAuthority().then(hasAuthority => getGeneralSettings(hasAuthority))
 }
