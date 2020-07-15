72d0009e7e8db273ade6ca0e45291070e52717a2
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2020-07-15 14:34:53 -0500

    fix: clean callbacks general settings

--- src/modules/general/apiLoadSettings.js
+++ src/modules/general/apiLoadSettings.js
@@ -3,39 +3,42 @@ import { GENERAL_SETTINGS, NAMESPACE } from '../../constants/data-store'
 import { getAuthority } from '../getAuthority'
 
 /**
- * Check if dataStore has ANDROID SETTINGS namespace and GENERAL_SETTINGS key, get general settings from dataStore
+ * Check:
+ * if dataStore has ANDROID SETTINGS namespace
+ * if dataStore has keys and GENERAL_SETTINGS key,
+ * Then: get general settings from dataStore
  * */
 const getGeneralSettings = async authority => {
     const hasAuthority = authority
 
     return api
         .getNamespaces()
-        .then(
-            namespaces =>
-                namespaces.includes(NAMESPACE) && api.getKeys(NAMESPACE)
-        )
-        .then(
-            keys =>
-                keys.includes(GENERAL_SETTINGS) &&
-                getSettingsFromDataStore(hasAuthority)
-        )
+        .then(namespaces => {
+            if (namespaces.includes(NAMESPACE)) {
+                return api.getKeys(NAMESPACE)
+            }
+        })
+        .then(keys => {
+            if (keys && keys.includes(GENERAL_SETTINGS)) {
+                return getSettingsFromDataStore(hasAuthority)
+            }
+        })
         .then(settings => settings)
+        .catch(e => e)
 }
 
 /**
  * Get general settings from dataStore and add disable flag in case user has authority
  * */
-const getSettingsFromDataStore = hasAuthority => {
-    return api.getValue(NAMESPACE, GENERAL_SETTINGS).then(settings => {
+const getSettingsFromDataStore = hasAuthority =>
+    api.getValue(NAMESPACE, GENERAL_SETTINGS).then(settings => {
         const generalSettings = settings.value
         generalSettings.disableAll = !hasAuthority
         return generalSettings
     })
-}
 
 /**
  * When general settings component mount, check if user has ALL authority, get namespace and keys from dataStore and return value
  */
-export const apiLoadGeneralSettings = async () => {
-    return getAuthority().then(hasAuthority => getGeneralSettings(hasAuthority))
-}
+export const apiLoadGeneralSettings = async () =>
+    getAuthority().then(hasAuthority => getGeneralSettings(hasAuthority))
