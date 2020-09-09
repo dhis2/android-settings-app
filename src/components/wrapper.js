00763d6027f1eaf2cde517553577f5a0c13d2afd
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2020-09-09 04:08:24 -0500

    fix: change material components to UI dataset general and specific

--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import cx from 'classnames'
 import styles from '../styles/TableActions.module.css'
 
 const Wrapper = props => (
-    <div className={styles.container}>{props.children}</div>
+    <div
+        className={cx(styles.container, {
+            [styles.fullContainer]: props.fullWidth,
+        })}
+    >
+        {props.children}
+    </div>
 )
 
 export default Wrapper
--- src/modules/dataset/parseValueBySettingType.js
+++ src/modules/dataset/parseValueBySettingType.js
@@ -7,7 +7,7 @@ export const parseValueBySettingType = (name, value) => {
     switch (name) {
         case PERIOD_DS_DOWNLOAD:
         case PERIOD_DS_DB_TRIMMING:
-            return value ? parseInt(value) : 0
+            return value <= 0 ? 0 : parseInt(value)
         default:
             return value
     }
