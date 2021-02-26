ee68b4ad9af3ae350d7728d4a98cd675b2dc83be
Milagros <sharmynrodriguez@gmail.com>
2021-02-25 23:59:26 -0500

    feat: basic page layout with header

---
+++ src/components/page/Page.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { CircularLoader } from '@dhis2/ui'
+import PropTypes from '@dhis2/prop-types'
+import styles from './Page.module.css'
+import UnsavedChangesAlert from '../unsaved-changes-alert'
+
+const Page = ({ loading, title, desc, unsavedChanges, children }) => {
+    if (loading === true) {
+        return <CircularLoader small />
+    }
+
+    return (
+        <>
+            <header>
+                <h1 className={styles.title}>{title}</h1>
+                <p className={styles.desc}>{desc}</p>
+            </header>
+
+            <div className={styles.content}>
+                <UnsavedChangesAlert unsavedChanges={unsavedChanges} />
+                {children}
+            </div>
+        </>
+    )
+}
+
+Page.propTypes = {
+    loading: PropTypes.bool,
+    title: PropTypes.string,
+    desc: PropTypes.string,
+    unsavedChanges: PropTypes.bool,
+    children: PropTypes.element,
+}
+
+export default Page
