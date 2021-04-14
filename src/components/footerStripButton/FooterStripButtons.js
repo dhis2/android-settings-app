03abf1de00a0ca10f1967385c366ecba88557dff
Milagros <sharmynrodriguez@gmail.com>
2021-04-13 20:38:51 -0500

    feat: button strip to save and reset settings

---
+++ src/components/footerStripButton/FooterStripButtons.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from '@dhis2/prop-types'
+import i18n from '@dhis2/d2-i18n'
+import { Button, ButtonStrip } from '@dhis2/ui'
+import buttonStyles from '../../styles/Button.module.css'
+import DialogSaveData from '../dialog/dialog-save-data'
+import SaveErrorAlert from '../alert-bar/save-error-alert'
+import SuccessAlert from '../alert-bar/success-alert'
+
+const FooterStripButtons = ({
+    saveButtonDisabled,
+    onReset,
+    onSave,
+    errorRequest,
+    requestResult,
+    handleDisableSave,
+    disableAll,
+}) => {
+    const [openDialog, setOpenDialog] = useState(false)
+
+    useEffect(() => {
+        if (requestResult) {
+            handleDisableSave(true)
+        }
+    }, [requestResult])
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false)
+    }
+
+    const handleSaveDatStore = () => {
+        onSave()
+        handleCloseDialog()
+    }
+
+    const handleOpenSaveDialog = () => {
+        setOpenDialog(true)
+    }
+
+    return (
+        <>
+            <ButtonStrip className={buttonStyles.container__padding}>
+                <Button
+                    primary
+                    className={buttonStyles.button_marginLeft}
+                    onClick={handleOpenSaveDialog}
+                    disabled={saveButtonDisabled}
+                >
+                    {i18n.t('Save')}
+                </Button>
+                <Button onClick={onReset} disabled={disableAll}>
+                    {i18n.t('Reset all values to default')}
+                </Button>
+            </ButtonStrip>
+
+            <DialogSaveData
+                openDialog={openDialog}
+                onClose={handleCloseDialog}
+                saveDataStore={handleSaveDatStore}
+            />
+
+            {requestResult && <SuccessAlert show={requestResult} />}
+
+            {errorRequest && (
+                <SaveErrorAlert show={true} message={errorRequest.message} />
+            )}
+        </>
+    )
+}
+
+FooterStripButtons.propTypes = {
+    saveButtonDisabled: PropTypes.bool,
+    onReset: PropTypes.func,
+    onSave: PropTypes.func,
+    handleDisableSave: PropTypes.func,
+}
+
+export default FooterStripButtons
