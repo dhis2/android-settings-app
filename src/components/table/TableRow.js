293531060afe5afbb82fdd7fa0f479b14f328334
Milagros <sharmynrodriguez@gmail.com>
2021-04-19 20:13:25 -0500

    feat: create basic checkbox and table for settings

---
+++ src/components/table/TableRow.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../../styles/TableSettings.module.css'
+
+export const TableRow = props => (
+    <div className={styles.parent}>{props.children}</div>
+)
