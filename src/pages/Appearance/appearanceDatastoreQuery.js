b40de0e80d81a5a90a6d2cab0b9950c9db2d3e97
Milagros <sharmynrodriguez@gmail.com>
2021-04-19 20:18:09 -0500

    feat: add queries and mutations for appearance key

---
+++ src/pages/Appearance/appearanceDatastoreQuery.js
@@ -0,0 +1,51 @@
+import { NAMESPACE } from '../../constants/data-store'
+import { useDataQuery } from '@dhis2/app-runtime'
+
+const APPEARANCE = 'appearance'
+
+/**
+ * update data store
+ * key: Appearance
+ * Settings that can be saved here: Home Screen, Programs, Dataset
+ * */
+
+export const saveAppearanceKeyMutation = {
+    resource: `dataStore/${NAMESPACE}/${APPEARANCE}`,
+    type: 'update',
+    data: ({ settings }) => ({
+        ...settings,
+        lastUpdated: new Date().toJSON(),
+    }),
+}
+
+/**
+ * Query to get Appearance Settings from Data store
+ * key: Appearance Settings
+ * */
+
+export const getAppearanceKeyQuery = {
+    appearanceSettings: {
+        resource: `dataStore/${NAMESPACE}/${APPEARANCE}`,
+    },
+}
+
+/**
+ * Read Appearance Data store values:
+ *
+ * */
+
+export const useReadAppearanceDataStore = () => {
+    const { loading, error, data } = useDataQuery(getAppearanceKeyQuery)
+
+    return {
+        load: loading,
+        error,
+        completionSpinner: data && data.appearanceSettings.completionSpinner,
+        filterSorting: data && data.appearanceSettings.filterSorting,
+        home: data && data.appearanceSettings.filterSorting.home,
+        programSettings:
+            data && data.appearanceSettings.filterSorting.programSettings,
+        dataSetSettings:
+            data && data.appearanceSettings.filterSorting.dataSetSettings,
+    }
+}
