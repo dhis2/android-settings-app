72e9dbbe98cc82904e2d3ad308193200e2d04cf7
Milagros <sharmynrodriguez@gmail.com>
2021-04-27 15:13:12 -0500

    feat: add page layout, save and reset

--- src/pages/Appearance/Home/HomeAppearance.js
+++ src/pages/Appearance/Home/HomeAppearance.js
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import { useDataQuery } from '@dhis2/app-runtime'
-import { useReadAppearanceDataStore } from '../appearanceDatastoreQuery'
+import i18n from '@dhis2/d2-i18n'
+import { useDataMutation, useDataQuery } from '@dhis2/app-runtime'
+import isEqual from 'lodash/isEqual'
+import Page from '../../../components/page/Page'
 import HomeSettings from './HomeSettings'
+import FooterStripButtons from '../../../components/footerStripButton/FooterStripButtons'
 import { createInitialValues } from './helper'
-
-const authorityQuery = {
-    authority: {
-        resource: 'me/authorities/ALL',
-    },
-}
+import {
+    saveAppearanceKeyMutation,
+    useReadAppearanceDataStore,
+} from '../appearanceDatastoreQuery'
+import { authorityQuery } from '../../../modules/apiLoadFirstSetup'
 
 const HomeAppearance = () => {
-    const { load, home } = useReadAppearanceDataStore()
+    const {
+        load,
+        home,
+        completionSpinner,
+        programSettings,
+        dataSetSettings,
+    } = useReadAppearanceDataStore()
     const { data } = useDataQuery(authorityQuery)
     const [settings, setSettings] = useState()
+    const [initialValues, setInitialValues] = useState()
     const [disable, setDisable] = useState(false)
+    const [disableSave, setDisableSave] = useState(true)
+
+    const [mutate, { error, data: updateResult }] = useDataMutation(
+        saveAppearanceKeyMutation
+    )
 
     useEffect(() => {
         data && setDisable(!data.authority)
@@ -22,22 +36,62 @@ const HomeAppearance = () => {
 
     useEffect(() => {
         if (home) {
+            setInitialValues(home)
             setSettings(createInitialValues(home))
         }
     }, [home])
 
+    useEffect(() => {
+        initialValues && settings && !isEqual(settings, initialValues)
+            ? setDisableSave(false)
+            : setDisableSave(true)
+    }, [settings])
+
+    const saveSettings = async () => {
+        const settingsToSave = {
+            completionSpinner,
+            filterSorting: {
+                home: settings,
+                programSettings,
+                dataSetSettings,
+            },
+        }
+        await mutate({ settings: settingsToSave })
+    }
+
+    const resetSettings = () => {
+        setSettings(createInitialValues(''))
+    }
+
     return (
-        <>
-            {settings && !load && (
+        <Page
+            title={i18n.t('Home Screen')}
+            desc={i18n.t(
+                'Customize the available filters and sorting options for the home screen. Filters and sorting options set here will only affect the home screen.'
+            )}
+            loading={load}
+            unsavedChanges={!disableSave}
+        >
+            {settings && (
                 <>
                     <HomeSettings
                         states={settings}
                         onChange={setSettings}
                         disable={disable}
                     />
+
+                    <FooterStripButtons
+                        onSave={saveSettings}
+                        onReset={resetSettings}
+                        saveButtonDisabled={disableSave}
+                        errorRequest={error}
+                        requestResult={updateResult}
+                        handleDisableSave={setDisableSave}
+                        disableAll={disable}
+                    />
                 </>
             )}
-        </>
+        </Page>
     )
 }
 
