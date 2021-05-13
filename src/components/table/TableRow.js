8fa8d87996368f3ac55a94265dbdcad04db49acc
Milagros <sharmynrodriguez@gmail.com>
2021-05-13 00:54:00 -0500

    style: add dense style for table rows

--- src/components/table/TableRow.js
+++ src/components/table/TableRow.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import PropTypes from '@dhis2/prop-types'
+import cx from 'classnames'
 import styles from '../../styles/TableSettings.module.css'
 
-export const TableRow = props => (
-    <div className={styles.parent}>{props.children}</div>
+export const TableRow = ({ children, dense }) => (
+    <div className={cx(styles.parent, { [styles.dense]: dense })}>
+        {children}
+    </div>
 )
+
+TableRow.propTypes = {
+    children: PropTypes.element,
+    dense: PropTypes.bool,
+}
