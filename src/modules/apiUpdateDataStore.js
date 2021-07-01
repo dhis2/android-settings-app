8d03f5b1feff90f35534716e90d6ba34773fcd2a
Milagros <sharmynrodriguez@gmail.com>
2021-06-30 22:05:28 -0500

    refactor: [sync programs] remove unused components

--- src/modules/apiUpdateDataStore.js
+++
@@ -1,6 +0,0 @@
-import api from '../utils/api'
-import { NAMESPACE } from '../constants/data-store'
-
-export const apiUpdateDataStore = async (data, keyName) => {
-    return await api.updateValue(NAMESPACE, keyName, data)
-}
--- src/modules/getAuthority.js
+++
@@ -1,8 +0,0 @@
-import { getInstance } from 'd2'
-
-export const getAuthority = () => {
-    return getInstance().then(d2 => {
-        const url = 'me/authorization/ALL'
-        return d2.Api.getApi().get(url)
-    })
-}
