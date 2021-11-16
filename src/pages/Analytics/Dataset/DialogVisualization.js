f2d219aa20f09cce3da245d2e9ee88ae17a8ca85
Milagros Rodríguez <sharmynrodriguez@gmail.com>
2021-11-16 09:47:34 -0500

    feat: add Data set visualization and groups (#100)
    * feat: [visualization] dialog to add visualization and group
    * feat: [visualization dataset] add visualization and groups

---
+++ src/pages/Analytics/Dataset/DialogVisualization.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+    Modal,
+    ModalTitle,
+    ModalContent,
+    ModalActions,
+    ButtonStrip,
+    Button,
+} from '@dhis2/ui'
+import i18n from '@dhis2/d2-i18n'
+import buttonStyles from '../../../styles/Button.module.css'
+import {
+    VisualizationGroup,
+    SelectDataset,
+    SelectVisualization,
+    VisualizationTitle,
+} from '../../../components/analyticVisualization'
+
+const DialogVisualization = ({
+    open,
+    settings,
+    handleChange,
+    disableSave,
+    handleClose,
+    handleSave,
+    groups,
+}) => (
+    <>
+        {open && (
+            <Modal large position="middle">
+                <ModalTitle>{i18n.t('Add Data set visualization')}</ModalTitle>
+
+                <ModalContent>
+                    <SelectDataset
+                        settings={settings}
+                        onChange={handleChange}
+                    />
+
+                    <SelectVisualization
+                        settings={settings}
+                        onChange={handleChange}
+                    />
+
+                    {settings.visualization && (
+                        <>
+                            <VisualizationTitle
+                                settings={settings}
+                                onChange={handleChange}
+                            />
+
+                            <VisualizationGroup
+                                settings={settings}
+                                onChange={handleChange}
+                                groupList={groups}
+                                type="dataset"
+                            />
+                        </>
+                    )}
+                </ModalContent>
+
+                <ModalActions>
+                    <ButtonStrip end>
+                        <Button
+                            onClick={handleClose}
+                            className={buttonStyles.mainContent__dialog__button}
+                        >
+                            {i18n.t('Cancel')}
+                        </Button>
+                        <Button onClick={handleSave} disabled={disableSave}>
+                            {i18n.t('Add Data set Visualization')}
+                        </Button>
+                    </ButtonStrip>
+                </ModalActions>
+            </Modal>
+        )}
+    </>
+)
+
+DialogVisualization.propTypes = {
+    open: PropTypes.bool,
+    settings: PropTypes.object,
+    handleChange: PropTypes.func,
+    disableSave: PropTypes.bool,
+    handleClose: PropTypes.func,
+    handleSave: PropTypes.func,
+    groups: PropTypes.object,
+}
+
+export default DialogVisualization
