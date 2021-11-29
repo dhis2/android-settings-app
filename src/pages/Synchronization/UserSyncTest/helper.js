713a6396e53abb8348fb775b63b408b6e8e29fb4
Milagros <sharmynrodriguez@gmail.com>
2021-11-29 13:52:25 -0500

    refactor: [user sync test] metadata and data download size helpers

--- src/pages/Synchronization/UserSyncTest/helper.js
+++ src/pages/Synchronization/UserSyncTest/helper.js
@@ -33,69 +33,142 @@ export const runUserTest = async ({
     globalSettings,
     specificSettings,
 }) => {
-    let orgUnitSearch = 0
-    let orgUnit = {}
-    let programs = {}
-    let dataSets = {}
-    let programRules = {}
-    let metadataSize = 0
-    let dataSize = 0
+    const {
+        orgUnitSearch,
+        orgUnit,
+        program,
+        dataSet,
+        programRule,
+    } = await getTestElements(user, dataEngine)
 
-    await getTestElements(user, dataEngine).then(result => {
-        orgUnitSearch = result.orgUnitSearch
-        orgUnit = result.orgUnit
-        programs = result.program
-        dataSets = result.dataSet
-        programRules = result.programRule
-    })
-
-    await getMetadataSize({
+    const metadataSize = await getMetadataSize({
         dataEngine,
         orgUnitList: orgUnit.idList,
-        programList: programs.idList,
-        trackedEntityTypeList: programs.trackedEntityTypeList,
-        teaList: programs.trackedEntityAttribute,
-        optionSetList: programs.optionSet,
-        dataSetList: dataSets.idList,
-        dataElementList: dataSets.dataElement,
-        categoryComboList: dataSets.categoryCombo,
-        categoryList: dataSets.category,
-        indicatorList: dataSets.indicator,
-    }).then(result => (metadataSize = result))
+        programList: program.idList,
+        trackedEntityTypeList: program.trackedEntityTypeList,
+        teaList: program.trackedEntityAttribute,
+        optionSetList: program.optionSet,
+        dataSetList: dataSet.idList,
+        dataElementList: dataSet.dataElement,
+        categoryComboList: dataSet.categoryCombo,
+        categoryList: dataSet.category,
+        indicatorList: dataSet.indicator,
+    })
 
-    await getDataSize({
+    const dataSize = await getDataSize({
         dataEngine,
         orgUnit: orgUnit.idList,
         program: specificSettings,
         globalLimit: globalSettings,
-    }).then(result => (dataSize = result))
+    })
 
     return {
         organisationUnitsNumber: orgUnit.total,
         organisationUnitSearchNumber: orgUnitSearch,
-        datasetNumber: dataSets.total,
-        programNumber: programs.total,
-        programRuleNumber: programRules.total,
+        datasetNumber: dataSet.total,
+        programNumber: program.total,
+        programRuleNumber: programRule.total,
         reservedValueNumber: reservedValues,
         metadataSize,
         dataSize,
     }
 }
 
-const getSearchOrgUnit = (orgUnits, dataEngine) => {
-    const orgUnitPromises = []
+const getSearchOrgUnit = async (orgUnits, dataEngine) => {
     const orgUnitSearch = []
-    orgUnits.map(orgUnit =>
-        orgUnitPromises.push(apiFetchOUSearch(dataEngine, orgUnit.id))
+    const orgUnitPromises = orgUnits.map(orgUnit =>
+        apiFetchOUSearch(dataEngine, orgUnit.id)
+    )
+    const ouLists = await Promise.all(orgUnitPromises)
+    ouLists.forEach(ouList =>
+        ouList.forEach(ou => {
+            orgUnitSearch.push(ou)
+        })
     )
 
-    return Promise.all(orgUnitPromises).then(result => {
-        result.map(ouList => ouList.map(ou => orgUnitSearch.push(ou)))
-        return uniqBy(orgUnitSearch, 'id').length
-    })
+    return uniqBy(orgUnitSearch, 'id').length
 }
 
 const parseUniqList = list => uniq(without(list, undefined))
+
+const getPrograms = orgUnitList => {
+    const programList = []
+    const trackedEntityTypeId = []
+    const trackedEntityAttributeId = []
+    const optionSetId = []
+
+    orgUnitList.forEach(({ programs }) =>
+        programs.forEach(
+            ({ id, trackedEntityType, programTrackedEntityAttributes }) => {
+                programList.push(id)
+                if (!isEmpty(trackedEntityType)) {
+                    trackedEntityTypeId.push(trackedEntityType.id)
+                }
+                if (!isEmpty(programTrackedEntityAttributes)) {
+                    programTrackedEntityAttributes.forEach(tea => {
+                        trackedEntityAttributeId.push(tea.id)
+                        if (!isEmpty(tea.trackedEntityAttribute.optionSet)) {
+                            optionSetId.push(
+                                tea.trackedEntityAttribute.optionSet.id
+                            )
+                        }
+                    })
+                }
+            }
+        )
+    )
+
+    return {
+        total: uniq(programList).length,
+        idList: uniq(programList),
+        trackedEntityType: parseUniqList(trackedEntityTypeId),
+        trackedEntityAttribute: parseUniqList(trackedEntityAttributeId),
+        optionSet: parseUniqList(optionSetId),
+    }
+}
+
+const getDatasets = orgUnitList => {
+    const dataSetList = []
+    const dataElementId = []
+    const categoryComboId = []
+    const categoriesId = []
+    const indicatorsId = []
+
+    orgUnitList.forEach(({ dataSets }) =>
+        dataSets.forEach(
+            ({ id, dataSetElements, indicators, categoryCombo }) => {
+                dataSetList.push(id)
+                if (!isEmpty(dataSetElements.dataElement)) {
+                    dataElementId.push(dataSetElements.dataElement.id)
+                }
+
+                if (!isEmpty(indicators)) {
+                    indicatorsId.push(
+                        ...indicators.map(indicator => indicator.id)
+                    )
+                }
+
+                if (!isEmpty(categoryCombo)) {
+                    categoryComboId.push(categoryCombo.id)
+                    categoriesId.push(
+                        ...categoryCombo.categories.map(
+                            categories => categories.id
+                        )
+                    )
+                }
+            }
+        )
+    )
+
+    return {
+        total: uniq(dataSetList).length,
+        idList: uniq(dataSetList),
+        dataElement: parseUniqList(dataElementId),
+        categoryCombo: parseUniqList(categoryComboId),
+        category: parseUniqList(categoriesId),
+        indicator: parseUniqList(indicatorsId),
+    }
+}
 
 /**
  * get programs, dataset and other elements from org unit
@@ -105,88 +178,25 @@ const parseUniqList = list => uniq(without(list, undefined))
  *
  * */
 const getOrgUnit = (orgUnits, dataEngine) => {
-    const orgUnitPromises = []
     const orgUnitCapture = []
-    orgUnits.map(orgUnit =>
-        orgUnitPromises.push(apiFetchOU(dataEngine, orgUnit.id))
+    const orgUnitPromises = orgUnits.map(orgUnit =>
+        apiFetchOU(dataEngine, orgUnit.id)
     )
 
     return Promise.all(orgUnitPromises).then(result => {
         result.map(ouList => ouList.map(ou => orgUnitCapture.push(ou)))
         const uniqOrgUnit = uniqBy(orgUnitCapture, 'id')
-        const orgUnitId = []
-        const optionSetId = []
-        const trackedEntityTypeId = []
-        const trackedEntityAttributeId = []
-        const dataElementId = []
-        const categoryComboId = []
-        const categoriesId = []
-        const indicatorsId = []
-        uniqOrgUnit.map(ou => orgUnitId.push(ou.id))
-
-        const programList = []
-        uniqOrgUnit.map(ou =>
-            ou.programs.map(
-                ({ id, trackedEntityType, programTrackedEntityAttributes }) => {
-                    programList.push(id)
-                    !isEmpty(trackedEntityType) &&
-                        trackedEntityTypeId.push(trackedEntityType.id)
-                    if (!isEmpty(programTrackedEntityAttributes)) {
-                        programTrackedEntityAttributes.map(tea => {
-                            trackedEntityAttributeId.push(tea.id)
-                            !isEmpty(tea.trackedEntityAttribute.optionSet) &&
-                                optionSetId.push(
-                                    tea.trackedEntityAttribute.optionSet.id
-                                )
-                        })
-                    }
-                }
-            )
-        )
-
-        const dataSetList = []
-        uniqOrgUnit.map(ou =>
-            ou.dataSets.map(
-                ({ id, dataSetElements, indicators, categoryCombo }) => {
-                    dataSetList.push(id)
-                    !isEmpty(dataSetElements.dataElement) &&
-                        dataElementId.push(dataSetElements.dataElement.id)
-
-                    if (!isEmpty(indicators)) {
-                        indicators.map(indicator =>
-                            indicatorsId.push(indicator.id)
-                        )
-                    }
-                    if (!isEmpty(categoryCombo)) {
-                        categoryComboId.push(categoryCombo.id)
-                        categoryCombo.categories.map(categories => {
-                            categoriesId.push(categories.id)
-                        })
-                    }
-                }
-            )
-        )
+        const orgUnitId = uniqOrgUnit.map(ou => ou.id)
+        const program = getPrograms(uniqOrgUnit)
+        const dataSet = getDatasets(uniqOrgUnit)
 
         return {
             orgUnit: {
                 total: uniqOrgUnit.length,
                 idList: orgUnitId,
             },
-            program: {
-                total: uniq(programList).length,
-                idList: uniq(programList),
-                trackedEntityType: parseUniqList(trackedEntityTypeId),
-                trackedEntityAttribute: parseUniqList(trackedEntityAttributeId),
-                optionSet: parseUniqList(optionSetId),
-            },
-            dataSet: {
-                total: uniq(dataSetList).length,
-                idList: uniq(dataSetList),
-                dataElement: parseUniqList(dataElementId),
-                categoryCombo: parseUniqList(categoryComboId),
-                category: parseUniqList(categoriesId),
-                indicator: parseUniqList(indicatorsId),
-            },
+            program,
+            dataSet,
         }
     })
 }
@@ -216,35 +226,18 @@ const getProgramRules = (programs, dataEngine) => {
 const getTestElements = async (user, dataEngine) => {
     const orgUnits = user.organisationUnits
     const orgUnitsSearch = user.teiSearchOrganisationUnits
-
-    let totalSearch = 0
-    let orgUnit = {}
-    let program = {}
-    let dataSet = {}
-    let programRule = {}
-
-    await getSearchOrgUnit(orgUnitsSearch, dataEngine).then(result => {
-        totalSearch = result
-    })
-
-    await getOrgUnit(orgUnits, dataEngine).then(result => {
-        orgUnit = { ...result.orgUnit }
-        program = { ...result.program }
-        dataSet = { ...result.dataSet }
-    })
-
-    await getProgramRules(program.idList, dataEngine).then(programRules => {
-        programRule = {
-            total: programRules.length,
-            idList: programRules,
-        }
-    })
+    const totalSearch = await getSearchOrgUnit(orgUnitsSearch, dataEngine)
+    const { orgUnit, program, dataSet } = await getOrgUnit(orgUnits, dataEngine)
+    const programRule = await getProgramRules(program.idList, dataEngine)
 
     return {
         orgUnitSearch: totalSearch,
         orgUnit,
         program,
-        programRule,
+        programRule: {
+            idList: programRule,
+            total: programRule.length,
+        },
         dataSet,
     }
 }
