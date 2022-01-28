43e2fb96bc7a5be6cd13bedf37e495ff4bfedea9
Milagros Rodríguez <sharmynrodriguez@gmail.com>
2022-01-28 08:47:38 -0500

    fix: valid URL has http or https protocol (#112)

--- src/utils/validators/__tests__/isValidURL.test.js
+++ src/utils/validators/__tests__/isValidURL.test.js
@@ -5,27 +5,32 @@ test('"https://debug.dhis2.org" is a valid URL', () => {
     expect(isValidURL(url)).toBeTruthy()
 })
 
-test('"www.google.com" is a valid URL', () => {
-    const url = 'www.google.com'
-    expect(isValidURL(url)).toBeTruthy()
-})
-
 test('"http://www.aa.com" is a valid URL', () => {
     const url = 'http://www.aa.com'
     expect(isValidURL(url)).toBeTruthy()
 })
 
-test('"apple.gr" is a valid URL', () => {
-    const url = 'apple.gr'
+test('"http://hoyhabloyo.com:8080/matomo.php" is a valid URL', () => {
+    const url = 'http://hoyhabloyo.com:8080/matomo.php'
     expect(isValidURL(url)).toBeTruthy()
 })
 
-test('"http://matomo.o3" is not a valid URL', () => {
+test('"http://matomo.o3" is a valid URL', () => {
     const url = 'http://matomo.o3'
-    expect(isValidURL(url)).toBeFalsy()
+    expect(isValidURL(url)).toBeTruthy()
 })
 
 test('"http://future" is not a valid URL', () => {
     const url = 'http://future'
+    expect(isValidURL(url)).toBeFalsy()
+})
+
+test('"www.google.com" is a not valid URL', () => {
+    const url = 'www.google.com'
+    expect(isValidURL(url)).toBeFalsy()
+})
+
+test('"apple.gr" is not a valid URL', () => {
+    const url = 'apple.gr'
     expect(isValidURL(url)).toBeFalsy()
 })
--- src/utils/validators/isValidURL.js
+++ src/utils/validators/isValidURL.js
@@ -1,6 +1,8 @@
 export const isValidURL = url => {
-    const res = url.match(
-        /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-    )
-    return res !== null
+    try {
+        const urlExp = /(https?:\/\/)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+        return url.match(urlExp)
+    } catch (error) {
+        return false
+    }
 }
