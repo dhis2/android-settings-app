1c4bd69d86cfea4aab11529f5e9783a1a5437c07
Milagros <sharmynrodriguez@gmail.com>
2022-06-13 13:59:49 -0400

    feat: improve visualization list

--- src/components/analyticVisualization/VisualizationSearch/visualizationQuery.js
+++ src/components/analyticVisualization/VisualizationSearch/visualizationQuery.js
@@ -2,7 +2,7 @@ export const getVisualizationsQuery = (query = '') => ({
     resource: `visualizations`,
     params: {
         query,
-        pageSize: 100,
+        pageSize: 20,
         fields:
             'id,displayName,type,relativePeriods,rowDimensions,columnDimensions,userOrganisationUnit,userOrganisationUnitChildren,userOrganisationUnitGrandChildren',
         order: 'displayName:asc',
