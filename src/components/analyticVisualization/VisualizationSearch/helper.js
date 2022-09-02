bab862e1e828ddcdf9da3fc1f114c987170306e7
Milagros Rodríguez <sharmynrodriguez@gmail.com>
2022-09-02 09:04:24 -0400

    fix: list hides visualizations with Today and Last 30 Days periods (#138)
    * fix: list hides visualizations with Today and Last 30 Days periods
    * fix: include relative period

--- src/components/analyticVisualization/VisualizationSearch/helper.js
+++ src/components/analyticVisualization/VisualizationSearch/helper.js
@@ -17,7 +17,7 @@ import {
     LAST_WEEK,
     LAST_YEAR,
     MONTHS_THIS_YEAR,
-    QUARTERS_THIS_YEAR,
+    QUARTERS_THIS_YEAR, THIS_DAY,
     THIS_MONTH,
     THIS_QUARTER,
     THIS_WEEK,
@@ -60,6 +60,7 @@ const checkVisualizationType = visualization => {
 
 const relativePeriodsList = [
     TODAY,
+    THIS_DAY,
     YESTERDAY,
     LAST_3_DAYS,
     LAST_7_DAYS,
