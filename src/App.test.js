fb4493aba36af9c6265d7a7c1f0b25aeaef68219
ismay <git.o8qty@simplelogin.com>
2022-09-06 12:35:58 +0200

    test(app): mock app layout component

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+/* eslint-disable-next-line react/display-name */
+jest.mock('./pages/AppLayout', () => () => {
+    return <mock-applayout />
+})
+
 it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<App />, div)
