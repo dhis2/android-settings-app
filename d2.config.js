25628e30d44ad2b4f2f944f10e55748cd93ac599
Milagros Rodríguez <sharmynrodriguez@gmail.com>
2023-06-03 15:28:48 +0200

    fix: change First launch dialog [DHIS2-14109] (#165)
    * fix: change First launch dialog [DHIS2-14109]
    * feat: update app version and description

--- d2.config.js
+++ d2.config.js
@@ -1,7 +1,8 @@
 const config = {
     title: 'Android Settings',
+    description:
+        'Configure synchronization parameters for the DHIS2 Android App, customize appearance, add TEI Analytics items.',
     type: 'app',
-
     entryPoints: {
         app: './src/App',
     },
