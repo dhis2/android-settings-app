b695d48dbebb056ce350212737d12e516a04c187
Milagros Rodríguez <sharmynrodriguez@gmail.com>
2023-11-22 13:23:26 +0100

    feat: create support for new/experimental features [DHIS2-16174] (#181)
    * feat: create more options component
    * feat: experimental features component and helper function
    * feat: add advanced settings to general
    * feat: change label title
    * feat: add useCallback to handleCheckbox function
    * fix: minor typo
    * fix: add useMemo to handleCheckbox function

---
+++ src/pages/General/experimentalFeatures/feature-list.js
@@ -0,0 +1,11 @@
+import i18n from '@dhis2/d2-i18n'
+
+export const featureList = [
+    {
+        name: 'newFormLayout',
+        label: i18n.t('New Data Entry Form'),
+        description: i18n.t(
+            'Enable the new data entry form with new inputs per value type. The inputs for all value types have been redesigned, with a improved selection mode and increased tappable areas and texts.'
+        ),
+    },
+]
