1085153678d42620eb9371cc0366522e1558029f
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2024-03-13 20:14:29 -0500

    feat: create bypass version and dialog component

---
+++ src/components/field/BypassDHIS2Version.js
@@ -0,0 +1,53 @@
+import i18n from '@dhis2/d2-i18n'
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+import DialogBypass from '../dialog/DialogBypass'
+import { CheckboxField } from './CheckboxField'
+
+const CODE = 'bypassDHIS2VersionCheck'
+
+export const defaultBypassDHIS2Version = false
+
+export const BypassDHIS2Version = ({ value, onChange, ...props }) => {
+    const [open, setOpen] = useState(false)
+
+    const handleCheckbox = () => {
+        setOpen(true)
+    }
+
+    const onCloseDialog = () => {
+        setOpen(false)
+    }
+
+    const handleBypassChange = (checked) => {
+        onChange({ ...value, [CODE]: !checked })
+        onCloseDialog()
+    }
+
+    return (
+        <>
+            <CheckboxField
+                name={CODE}
+                label={i18n.t('Bypass DHIS2 Version')}
+                helpText={i18n.t(
+                    'Bypass the validation process for DHIS2 version compatibility'
+                )}
+                checked={value[CODE]}
+                onChange={handleCheckbox}
+                {...props}
+            />
+
+            <DialogBypass
+                openDialog={open}
+                handleBypass={handleBypassChange}
+                onClose={onCloseDialog}
+                checked={value[CODE]}
+            />
+        </>
+    )
+}
+
+BypassDHIS2Version.propTypes = {
+    value: PropTypes.object,
+    onChange: PropTypes.func,
+}
