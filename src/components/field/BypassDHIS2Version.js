bf23c95e9795ce8cdd02cca6af37f52c746c83db
Milagros <sharmynrodriguez@gmail.com>
2024-05-24 13:01:19 -0500

    feat: add users info as help text

--- src/components/field/BypassDHIS2Version.js
+++ src/components/field/BypassDHIS2Version.js
@@ -1,8 +1,54 @@
 import i18n from '@dhis2/d2-i18n'
+import { CheckboxField as UICheckboxField } from '@dhis2/ui'
+import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 import DialogBypass from '../dialog/DialogBypass'
-import { CheckboxField } from './CheckboxField'
+import { FieldSection } from './FieldSection'
+
+const HelpText = ({ helpText, warning, type }) => {
+    if (!warning) {
+        return helpText
+    }
+
+    return (
+        <>
+            <span>
+                {helpText}
+                <span
+                    className={cx('prefix', {
+                        prefixInfo: type === 'info',
+                    })}
+                >
+                    {warning}
+                </span>
+            </span>
+
+            <style>{`
+                .prefix {
+                font - family: monospace;
+                font-size: 13px;
+                color: var(--colors-grey800);
+                background: var(--colors-grey300);
+                padding: 2px var(--spacers-dp4);
+                margin-right: var(--spacers-dp4);
+                border-radius: 3px;
+            }
+
+                .prefixInfo {
+                color: var(--colors-blue800);
+                background: var(--colors-blue050);
+            }
+            `}</style>
+        </>
+    )
+}
+
+HelpText.propTypes = {
+    helpText: PropTypes.string,
+    warning: PropTypes.string,
+    type: PropTypes.string,
+}
 
 const CODE = 'bypassDHIS2VersionCheck'
 
@@ -26,16 +72,27 @@ export const BypassDHIS2Version = ({ value, onChange, ...props }) => {
 
     return (
         <>
-            <CheckboxField
-                name={CODE}
-                label={i18n.t('Skip DHIS2 version validation')}
-                helpText={i18n.t(
-                    'Bypass the validation process for DHIS2 version compatibility'
-                )}
-                checked={value[CODE]}
-                onChange={handleCheckbox}
-                {...props}
-            />
+            <FieldSection>
+                <UICheckboxField
+                    label={i18n.t('Skip DHIS2 version validation')}
+                    name={CODE}
+                    checked={value[CODE]}
+                    type="checkbox"
+                    onChange={handleCheckbox}
+                    helpText={
+                        <HelpText
+                            helpText={i18n.t(
+                                'Bypass the validation process for DHIS2 version compatibility.'
+                            )}
+                            warning={i18n.t(
+                                'Only applicable for users using Android app version 3.0 or later.'
+                            )}
+                            type="info"
+                        />
+                    }
+                    {...props}
+                />
+            </FieldSection>
 
             <DialogBypass
                 openDialog={open}
