4a4e24cff1ceeed5640a5c66f2bb18fb2f170d4e
Chisomchima <chimachisom360@gmail.com>
2025-05-14 06:55:53 +0100

    feat: implement dynamic request parameters management in RequestForm

--- src/components/field/TextField.js
+++ src/components/field/TextField.js
@@ -3,20 +3,19 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { FieldSection } from './FieldSection'
 
-export const TextField = ({ label, helpText, value, required, ...props }) => {
-    console.log({props})
-    return(
-    <FieldSection>
-        <InputField
-            inputWidth="300px"
-            helpText={helpText}
-            label={label}
-            value={value}
-            {...props}
-        />
-    </FieldSection>
-)}
-
+export const TextField = ({ label, helpText, value, ...props }) => {
+    return (
+        <FieldSection>
+            <InputField
+                inputWidth="300px"
+                helpText={helpText}
+                label={label}
+                value={value}
+                {...props}
+            />
+        </FieldSection>
+    )
+}
 
 TextField.propTypes = {
     label: PropTypes.string,
