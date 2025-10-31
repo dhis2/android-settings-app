ea3e69b4338bf1f80d66cfbc606d68fc5f5b7c1c
Milagros <milagros@dhis2.org>
2025-10-31 12:59:23 +0100

    feat: get tei and events filters

---
+++ src/pages/Synchronization/Programs/ProgramQueries.js
@@ -0,0 +1,100 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+import { useEffect, useState } from 'react'
+import { filterListByReadAccess } from '../../../utils/utils'
+import { useWorkflowContext } from '../../../workflow-context'
+
+/**
+ * Combine multiple arrays of objects and group them by program id
+ * returns Grouped object by program id
+ */
+const groupByProgramId = (...arrays) => {
+    return arrays.flat().reduce((acc, item) => {
+        const programId =
+            typeof item?.program === 'string'
+                ? item?.program
+                : item?.program?.id
+
+        if (!programId) {
+            return acc
+        }
+
+        if (!acc[programId]) {
+            acc[programId] = []
+        }
+
+        acc[programId].push(item)
+
+        return acc
+    }, {})
+}
+
+/**
+ * Combines a list of programs with their corresponding filters
+ * returns A new array of programs including filters where applicable
+ */
+const combineProgramsWithFilters = (programsList, filtersByProgramId) => {
+    if (
+        !Array.isArray(programsList) ||
+        typeof filtersByProgramId !== 'object'
+    ) {
+        return []
+    }
+
+    return programsList.map((program) => {
+        const filters = filtersByProgramId[program.id] || []
+        return filters.length ? { ...program, filters } : { ...program }
+    })
+}
+
+/**
+ * Query to get program list with attributes and indicators
+ * */
+
+const query = {
+    trackedEntityInstanceFilters: {
+        resource: 'trackedEntityInstanceFilters',
+        params: {
+            fields: ['id', 'displayName', 'name', 'program[id]'],
+            paging: false,
+        },
+    },
+    programStageWorkingLists: {
+        resource: 'programStageWorkingLists',
+        params: {
+            fields: ['id', 'displayName', 'name', 'program[id]'],
+            paging: false,
+        },
+    },
+    eventFilters: {
+        resource: 'eventFilters',
+        params: {
+            fields: ['id', 'displayName', 'name', 'program'],
+            paging: false,
+        },
+    },
+}
+
+export const useProgramFilters = () => {
+    const { data } = useDataQuery(query)
+    const { programs } = useWorkflowContext()
+    const [programFilterList, setProgramList] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        if (data) {
+            const tei =
+                data?.trackedEntityInstanceFilters?.trackedEntityInstanceFilters
+            const programWorking =
+                data?.programStageWorkingList?.programStageWorkingList
+            const event = data?.eventFilters?.eventFilters
+
+            const filters = groupByProgramId(tei, programWorking, event)
+            const programL = filterListByReadAccess(programs)
+
+            setProgramList(combineProgramsWithFilters(programL, filters))
+            setLoading(true)
+        }
+    }, [data])
+
+    return { programFilterList, loading }
+}
