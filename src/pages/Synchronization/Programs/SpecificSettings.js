3d01cf1ffd5f0a976fabe29f2e2eca1b10f12336
Milagros <milagros@dhis2.org>
2025-11-01 18:18:19 +0100

    feat: add program filters

--- src/pages/Synchronization/Programs/SpecificSettings.js
+++ src/pages/Synchronization/Programs/SpecificSettings.js
@@ -2,7 +2,11 @@ import i18n from '@dhis2/d2-i18n'
 import { Divider } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React from 'react'
-import { InputNumber, SelectSettings } from '../../../components/inputs'
+import {
+    InputNumber,
+    MultiSelect,
+    SelectSettings,
+} from '../../../components/inputs'
 import { TableRow } from '../../../components/table/'
 import Wrapper from '../../../components/Wrapper'
 import {
@@ -11,8 +15,61 @@ import {
     TEI_DOWNLOAD,
 } from '../../../constants/program-settings'
 import tableTitleStyles from '../../../styles/TableTitle.module.css'
+import { isValidValue } from '../../../utils/validators'
 
-const ProgramTableRow = ({ row, specificSetting, onChange }) => (
+const getProgramFiltersById = (programId, list) => {
+    const program = list?.find((p) => p.id === programId)
+    return program?.filters
+}
+
+const SelectField = ({
+    singleSelect,
+    row,
+    onChange,
+    specificSetting,
+    filters,
+}) => {
+    const emptyList = !isValidValue(filters) || filters?.length === 0
+
+    return (
+        <>
+            {singleSelect ? (
+                <SelectSettings
+                    key={row.keyDownload}
+                    onChange={onChange}
+                    keyDownload={row.keyDownload}
+                    value={specificSetting[row.keyDownload]}
+                    options={row.download}
+                />
+            ) : (
+                <MultiSelect
+                    filterable
+                    disabled={emptyList}
+                    placeholder={
+                        emptyList
+                            ? i18n.t('No filters available')
+                            : i18n.t('Choose a filter')
+                    }
+                    empty={i18n.t('No filters available')}
+                    key={row.keyDownload}
+                    onChange={(e) => onChange(e, row.keyDownload)}
+                    selected={specificSetting[row.keyDownload]}
+                    options={filters || []}
+                />
+            )}
+        </>
+    )
+}
+
+SelectField.propTypes = {
+    singleSelect: PropTypes.bool,
+    row: PropTypes.object,
+    specificSetting: PropTypes.object,
+    onChange: PropTypes.func,
+    filters: PropTypes.array,
+}
+
+const ProgramTableRow = ({ row, specificSetting, onChange, filters }) => (
     <>
         <TableRow dense>
             <>
@@ -29,12 +86,12 @@ const ProgramTableRow = ({ row, specificSetting, onChange }) => (
                 </div>
                 <div>
                     {Array.isArray(row.download) ? (
-                        <SelectSettings
-                            key={row.keyDownload}
+                        <SelectField
+                            singleSelect={!row.multiple}
+                            row={row}
                             onChange={onChange}
-                            keyDownload={row.keyDownload}
-                            value={specificSetting[row.keyDownload]}
-                            options={row.download}
+                            specificSetting={specificSetting}
+                            filters={filters}
                         />
                     ) : (
                         <InputNumber
@@ -62,6 +119,7 @@ const SpecificSettings = ({
     specificSetting,
     onChange,
     programWithRegistration,
+    programOptions,
 }) => (
     <Wrapper fullWidth>
         <div>
@@ -72,6 +130,10 @@ const SpecificSettings = ({
                           row={row}
                           specificSetting={specificSetting}
                           onChange={onChange}
+                          filters={getProgramFiltersById(
+                              specificSetting.id,
+                              programOptions
+                          )}
                       />
                   ))
                 : SpecificProgram.withoutRegistration.map((row) => (
@@ -80,6 +142,10 @@ const SpecificSettings = ({
                           row={row}
                           specificSetting={specificSetting}
                           onChange={onChange}
+                          filters={getProgramFiltersById(
+                              specificSetting.id,
+                              programOptions
+                          )}
                       />
                   ))}
         </div>
