1a16eb8d1195ab6569b4392592f254203e3c0878
Milagros <milagros@dhis2.org>
2025-11-01 18:21:11 +0100

    feat: save program filters

--- src/pages/Synchronization/Programs/ProgramSyncSettings.js
+++ src/pages/Synchronization/Programs/ProgramSyncSettings.js
@@ -15,6 +15,7 @@ import {
     saveSynchronizationKeyMutation,
     useGetSyncDataStore,
 } from '../SyncDatastoreQuery'
+import { transformSpecificSettingsDataStore } from './helper'
 import { populateProgramObject } from './populateProgramObject'
 import ProgramGlobalSettings from './ProgramGlobalSettings'
 import ProgramSpecificSettings from './ProgramSpecificSettings'
@@ -80,7 +81,8 @@ const ProgramSyncSettings = () => {
             dataSetSettings,
             programSettings: {
                 globalSettings,
-                specificSettings,
+                specificSettings:
+                    transformSpecificSettingsDataStore(specificSettings),
             },
         }
 
